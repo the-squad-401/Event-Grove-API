dd8b8cc3bd7604591ab0744c81488d58635e1eb7
schwamman <schwamman@msn.com>
2019-10-15 17:01:07 -0500

    Added event emitter

---
+++ src/event-emitter.js
@@ -0,0 +1,54 @@
+'use strict';
+require('dotenv').config();
+
+const EMAIL_PROVIDER = process.env.EMAIL_PROVIDER || 'abc.123@xyz.com';
+
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const Categories = require('./models/category/category');
+const Businesses = require('./models/business/business');
+const Users = require('./models/user/user');
+
+const categories = new Categories();
+const businesses = new Businesses();
+const users = new Users();
+
+
+async function emitNotifications(event) {
+  const businessId = event.business;
+  const categoryId = event.category;
+  console.log(business, category);
+
+  const business = await businesses.get(businessId);
+  const category = await categories.get(categoryId);
+  const visited = {};
+
+  for (const userId of business.subscribers) {
+    sendNotification(userId, event);
+    visited[userId] = true;
+  }
+  for (const userId of category.subscribers) {
+    if (userId in visited) {
+      continue;
+    }
+    sendNotification(userId, event);
+  }
+}
+
+function createEmail(user, event) {
+  return {
+    to: user.email,
+    from: EMAIL_PROVIDER,
+    subject: event.name,
+    html: `${event.description}`,
+  };
+}
+
+function sendNotification(user, event) {
+  user = users.get(user);
+  sgMail.send(createEmail(user, event));
+}
+
+module.exports = emitNotifications;
