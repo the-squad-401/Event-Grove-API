da752e965f3e3e93a858427d36a50e56b73d1e5e
Calvin G Hall <123chall@gmail.com>
2019-10-15 10:58:01 -0500

    added mongo stuff to index

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
 
+require('dotenv').config();
+
+// Start DB Server
+const mongoose = require('mongoose');
+const mongooseOptions = {
+  useNewUrlParser:true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+
 let PORT = process.env.PORT || 3000;
 
 require('./src/server').start(PORT);
