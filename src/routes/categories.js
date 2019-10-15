d3e7697e3bbcfa1105ac14d97cecd8102b8de8f7
Jacob-Wendt <jacob.wendt@yhaoo.com>
2019-10-15 14:52:03 -0500

    added routes for categories along with tests

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -10,21 +10,44 @@ var mongoose = require('mongoose');
 
 router.get('/categories', handleGet);
 router.post('/category', handlePost);
-// categories.put('/category', handlePut);
-// categories.delete('/category', handleDelete);
+router.put('/category/:id', handlePut);
+router.delete('/category/:id', handleDelete);
+
+function get404(id) {
+  const error = new Error(`No category found with id: ${id}`);
+  error.status = 404;
+  return error;
+}
+
+function sendRecord(record, id, res, next) {
+  if (!record) {
+    next(get404(id));
+  } else {
+    res.status(200).json(record);
+  }
+}
 
 async function handleGet(req, res, next) {
-  let findAll = await router.find();
-  res.status(200).json(findAll);
+  res.status(200).json(await modelRepository.get());
 }
 
 async function handlePost(req, res, next) {
-  modelRepository
-    .post(req.body)
-    .then(newCategory => {
-      res.status(201)
-        .send(newCategory);
-    });
+  try {
+    let record = await modelRepository.post(req.body);
+    res.status(201).send(record);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function handlePut(req, res, next) {
+  const record = await modelRepository.put(req.params.id, req.body);
+  sendRecord(record, req.params.id, res, next);
+}
+
+async function handleDelete(req, res, next) {
+  let record = await modelRepository.delete(req.params.id);
+  sendRecord(record, req.params.id, res, next);
 }
 
 module.exports = router;
