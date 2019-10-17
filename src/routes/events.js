9cf5e7a73502610b695e96c48f904233ddaec037
Calvin G Hall <123chall@gmail.com>
2019-10-17 13:40:34 -0500

    Added GET user route, fixed event emission

--- src/routes/events.js
+++ src/routes/events.js
@@ -14,6 +14,8 @@ const { wrap, get401, verifyExists, send } = require('../route-helpers');
 
 const events = new Events();
 
+const emitEvent = require('../event-emitter');
+
 router.get('/events', wrap(getEvents));
 router.get('/events/:id', wrap(getEventById));
 router.get('/events/:category', wrap(getEventsByCategory));
@@ -44,7 +46,7 @@ async function getEvents(req, res) {
 
 /**
  * Retrieves and sends back a single event via ID
- * @route GET /event/{id}
+ * @route GET /events/{id}
  * @param {string} id.path.required - ID of event to GET
  * @returns {Event.model} 200 - an object containing the information of an event.
  * @returns {Error} 404 - event with ID could not be found.
@@ -57,7 +59,7 @@ async function getEventById(req, res) {
 
 /**
  * Retrieves and sends back all the events in a certain category via ID
- * @route GET /event/{category} 
+ * @route GET /events/{category} 
  * @param {string} category.path.required - Category ID of event to GET
  * @returns {object} 200 - an object containing the information of an event within a category.
  */
@@ -76,7 +78,7 @@ async function authOwner(tokenData, businessId) {
 
 /**
  * Creates and sends back a new event from JSON in the req.body
- * @route POST /event
+ * @route POST /events
  * @param {Event.model} event.body.required - creates a new event
  * @returns {Event.model} 200 - an event is created with the information submitted
  * @security Bearer
@@ -85,12 +87,13 @@ async function postEvent(req, res) {
   const tokenData = jwt.decode(req.token);
   await authOwner(tokenData, req.body.business);
   const record = await events.post(req.body);
+  emitEvent(record);
   send(record, res, 201);
 }
 
 /**
  * Updates and sends back the new event via ID
- * @route PUT /event/{id}
+ * @route PUT /events/{id}
  * @param {string} id.path.required - ID of the event to PUT
  * @param {object} update.body.required - Updated info
  * @returns {Event.model} 200 - an object containing the information of the updated event
@@ -107,7 +110,7 @@ async function updateEvent(req, res) {
 
 /**
  * Deletes and sends back, for the last time, a event via ID
- * @route DELETE /event/{id}
+ * @route DELETE /events/{id}
  * @param {string} id.path.required - ID of the event to DELETE
  * @returns {Event.model} 200 - an object no longer containing the information for an event
  * @returns {Error} 404 - event with ID could not be found
