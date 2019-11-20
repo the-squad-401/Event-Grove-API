1b3a41253a143d8e89aa51d05bb0931955e1cc47
Zscoob <james.zobian@gmail.com>
2019-11-20 15:01:49 -0600

    mockEvents.js

---
+++ docs/mockEvent.js
@@ -0,0 +1,688 @@
+const event1 = {
+  business: 'SolHair',
+  category: 'Services',
+  description: 'Before 2pm enjoy a haircut with a free wash and blow for only $35!',
+  startDate: ,
+  endDate: ,
+  image: '35.jpeg',
+};
+
+const event2 = {
+  business: 'SolHair',
+  category: 'Services',
+  description: 'Free wash and cut for our elderly customers!',
+  startDate: ,
+  endDate: ,
+  image: 'free-hair-wash-and-cut-service.jpeg',
+};
+
+const event3 = {
+  business: 'SolHair',
+  category: 'Services',
+  description: 'Free wash, cut, and blowdry with every coloring service!',
+  startDate: ,
+  endDate: ,
+  image: 'washandcut.jpeg',
+};
+
+const event4 = {
+  business: 'Grilled Cheese R Us',
+  category: 'Dining',
+  description: '$5 off any entree!',
+  startDate: ,
+  endDate: ,
+  image: '5offanyentree.jpeg',
+};
+
+const event5 = {
+  business: 'Grilled Cheese R Us',
+  category: 'Dining',
+  description: 'Slow day!!! 30% off!!!',
+  startDate: ,
+  endDate: ,
+  image: '30percentoff.jpeg',
+};
+
+const event6 = {
+  business: 'Grilled Cheese R Us',
+  category: 'Dining',
+  description: 'Buy one grilled cheese, get the second for $1!!!',
+  startDate: ,
+  endDate: ,
+  image: 'buyonegetone.jpeg',
+};
+
+const event7 = {
+  business: 'MetalHorse',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event8 = {
+  business: 'MetalHorse',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event9 = {
+  business: 'MetalHorse',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event10 = {
+  business: 'PoPcOrNiCeCrEaM',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event11 = {
+  business: 'PoPcOrNiCeCrEaM',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event12 = {
+  business: 'PoPcOrNiCeCrEaM',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event13 = {
+  business: 'The Pub Room',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event14 = {
+  business: 'The Pub Room',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event15 = {
+  business: 'The Pub Room',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event16 = {
+  business: 'The Mic',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event17 = {
+  business: 'The Mic',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event18 = {
+  business: 'The Mic',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event19 = {
+  business: 'Unicorn Wear',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event20 = {
+  business: 'Unicorn Wear',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event21 = {
+  business: 'Unicorn Wear',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event22 = {
+  business: 'Drill Baby Drill',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event23 = {
+  business: 'Drill Baby Drill',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event24 = {
+  business: 'Drill Baby Drill',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event25 = {
+  business: 'Brewhalla',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event26 = {
+  business: 'Brewhalla',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event27 = {
+  business: 'Brewhalla',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event28 = {
+  business: 'Surf n Turf',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event29 = {
+  business: 'Surf n Turf',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event30 = {
+  business: 'Surf n Turf',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event31 = {
+  business: 'projection nation',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event32 = {
+  business: 'projection nation',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event33 = {
+  business: 'projection nation',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event34 = {
+  business: 'The Vinyl',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event35 = {
+  business: 'The Vinyl',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event36 = {
+  business: 'The Vinyl',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event37 = {
+  business: 'NailSpa',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event38 = {
+  business: 'NailSpa',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event39 = {
+  business: 'NailSpa',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event40 = {
+  business: 'Karaoke CRapids',
+  category: 'Entertainmen',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event41 = {
+  business: 'Karaoke CRapids',
+  category: 'Entertainmen',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event42 = {
+  business: 'Karaoke CRapids',
+  category: 'Entertainmen',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event43 = {
+  business: 'The Atlas Room',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event44 = {
+  business: 'The Atlas Room',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event45 = {
+  business: 'The Atlas Room',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event46 = {
+  business: 'Locksmith Smithy',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event47 = {
+  business: 'Locksmith Smithy',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event48 = {
+  business: 'Locksmith Smithy',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event49 = {
+  business: 'Very Good Construction',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event50 = {
+  business: 'Very Good Construction',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event51 = {
+  business: 'Very Good Construction',
+  category: 'Services',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event52 = {
+  business: 'Newish DarkBlue',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event53 = {
+  business: 'Newish DarkBlue',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event54 = {
+  business: 'Newish DarkBlue',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event55 = {
+  business: 'Pizza Kings',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event56 = {
+  business: 'Pizza Kings',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event57 = {
+  business: 'Pizza Kings',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event58 = {
+  business: 'The LifeCycle',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event59 = {
+  business: 'The LifeCycle',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event60 = {
+  business: 'The LifeCycle',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event61 = {
+  business: 'Downtown Theater Group',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event62 = {
+  business: 'Downtown Theater Group',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event63 = {
+  business: 'Downtown Theater Group',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event64 = {
+  business: 'Ravers Paradise',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event65 = {
+  business: 'Ravers Paradise',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event66 = {
+  business: 'Ravers Paradise',
+  category: 'Entertainment',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event67 = {
+  business: 'Tigers Den',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event68 = {
+  business: 'Tigers Den',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event69 = {
+  business: 'Tigers Den',
+  category: 'Bar',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event70 = {
+  business: 'Locally Sourced',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event71 = {
+  business: 'Locally Sourced',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event72 = {
+  business: 'Locally Sourced',
+  category: 'Shopping',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event73 = {
+  business: 'Doughnut Madness',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event74 = {
+  business: 'Doughnut Madness',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+const event75 = {
+  business: 'Doughnut Madness',
+  category: 'Dining',
+  description: '',
+  startDate: ,
+  endDate: ,
+  image: '',
+};
+
+ 
+
+
+
+
+// const event = {
+//   business: '',
+//   category: '',
+//   description: '',
+//   startDate: ,
+//   endDate: ,
+//   image: '',
+// }; 
